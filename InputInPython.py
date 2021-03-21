7ab5c8ba418dbcbbcda2d1fb978b5f987a39ae79
h-jaiswal <54812359+h-jaiswal@users.noreply.github.com>
2021-03-21 09:22:36 +0530

    Day 2

---
+++ InputInPython.py
@@ -0,0 +1,49 @@
+
+# type() gives data type of whatever we pass into it
+
+# eg. type( name ) will give data type of name variable
+
+# eg. type(x) will give data type of x variable
+
+# eg. type(5) will give data type of 5 which is int (i.e integer)
+
+# eg. type("Hello") will give data type of 5 which is string 
+
+# eg. type(5.95) will give data type of 5.95 which is float ( ie floating point number ie decimal number)
+
+
+
+
+
+
+
+# input in python is taken using   input()
+
+
+# by default, input() takes string input
+
+# below line will take STRING input from user and store it inside of a variable called userInput # NOTE: name of a variable can be ANY WORD
+
+userInput = input()
+
+# below line will first detect data type of variable named userInput using type()
+# and then display it using print()
+
+print( type(userInput) )
+
+# NOTE: whatever input is given by user, input() will by default take it as STRING INPUT
+
+# Harshit Jaiswal
+# <class 'str'>
+
+# 200
+# <class 'str'>
+
+42.89
+<class 'str'>
+
+
+
+
+
+
---
+++ InputWithMessage.py
@@ -0,0 +1,19 @@
+# to display a message to user when taking input
+
+name = input("Enter a name: ")
+
+print(name)
+
+
+
+# age = input("Enter a age: (Numbers only) : ")
+
+age = int( input("Enter a age: (Numbers only) : ") )
+
+print(age)
+
+
+
+marks = input( "Enter marks scored last year: ")
+
+print(marks) 
