3f40646539ff17f6a939a520846bf234c4966734
h-jaiswal <mr.harshit.jaiswal@gmail.com>
2021-08-28 18:26:33 +0530

    range function

---
+++ REVISION CONTENT/7. range function/1. use in for loop.py
@@ -0,0 +1,32 @@
+def main():
+
+    # range function with only one argument
+    # start = 0  (default)
+    # end = argument
+    # jump = 1  (default)
+    # range(10) ->  0, 1, 2, 3, 4, 5, 6, 7, 8, 9 END
+    # below loop runs 10 times
+    for i in range(10):
+        print(i)
+
+    print()
+
+    for i in range(0, 10):
+        print(i)
+
+    print()
+
+    for i in range(5, 20):
+        print(i)
+
+    print()
+
+    for i in range(5, 30, 7):
+        print(i)
+
+    print()
+
+    for i in range(10, 0, -1):
+        print(i)
+
+main()
---
+++ REVISION CONTENT/7. range function/2. use to check range.py
@@ -0,0 +1,22 @@
+"""
+If we want to check if a number N lies between START(inclusive) and END(exclusive)
+
+ex to check  if  10 lies between   [7, 12)  i.e in range(7, 12) i.e. in {7, 8, 9, 10, 11}
+
+    10 in range(7, 12)  expression will return TRUE if 10 lies in {7, 8, 9, 10, 11} otherwise return FALSE
+
+"""
+
+def main():
+    num = int(input("Enter a number:"))
+
+    # code to check if num lies in range {100, 101, 102, ............................., 999}
+    # i.e. to check if num lies in range(100, 1000)     (100 inclusive, 1000 exlusive)
+
+
+    if (num in range(100, 1000)):
+        print("Number lies in range {100, 101, 102, ............................., 999}")
+    else:
+        print("Number DO NOT lies in range {100, 101, 102, ............................., 999}")
+
+main()
